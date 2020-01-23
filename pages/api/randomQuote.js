bee2b4b25de00a6252b4de0c323018ce1ec566ee
Manohar Kurapati <kvmanohar@gmail.com>
2020-01-23 21:21:24 +0000

    api with params completed

--- pages/api/randomQuote.js
+++ pages/api/randomQuote.js
@@ -1,6 +1,16 @@
 import allQuotes from '../../quotes.json';
 
 export default (req, res) => {
-	const quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+	const { author } = req.query;
+	let quotes = allQuotes;
+
+	if (author) {
+		quotes = quotes.filter((quote) => quote.author.toLowerCase().includes(author.toLowerCase()));
+	}
+	if (!quotes.length) {
+		quotes = allQuotes.filter((quote) => quote.author.toLowerCase() === 'unknown');
+	}
+
+	const quote = quotes[Math.floor(Math.random() * quotes.length)];
 	res.status(200).json(quote);
 };
